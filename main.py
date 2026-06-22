19572c651fc4f06ffdd626e5cd064ec19121eee2
jit <jittiza2005@gmail.com>
2026-06-21 20:12:34 -0700

    Anime squadron 0.5 update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ os.makedirs(SAVE_FOLDER, exist_ok=True)
 LAST_DONE_FILE = os.path.join(SAVE_FOLDER, "last_done.txt")
 AIZEN_STATE_FILE = os.path.join(SAVE_FOLDER, "aizen_state.txt")
 SHENRON_STATE_FILE = os.path.join(SAVE_FOLDER, "shenron_state.txt")
+GRIFFITH_STATE_FILE = os.path.join(SAVE_FOLDER, "griffith_state.txt")
 
 
 def check_stop():
@@ -41,6 +42,7 @@ def direct_click(x, y, clicks=1):
         pydirectinput.mouseUp()
         safe_sleep(0.15)
 
+
 def wait_for_pixels(pixel1=(0, 0, 255, 255, 255), tolerance=20):
     x, y, r, g, b = pixel1
 
@@ -51,6 +53,7 @@ def wait_for_pixels(pixel1=(0, 0, 255, 255, 255), tolerance=20):
             return True
 
         time.sleep(0.2)
+
 
 def check_for_pixels(pixel1=(0, 0, 255, 255, 255), timeout=5):
     x, y, r, g, b = pixel1
@@ -65,6 +68,29 @@ def check_for_pixels(pixel1=(0, 0, 255, 255, 255), timeout=5):
         time.sleep(0.2)
 
     return False
+
+
+def check_for_any_pixels(pixels, timeout=5):
+    end_time = time.time() + timeout
+
+    while time.time() < end_time:
+        check_stop()
+
+        for pixel in pixels:
+            x, y, r, g, b = pixel
+
+            if pag.pixelMatchesColor(
+                x,
+                y,
+                (r, g, b),
+                tolerance=20
+            ):
+                return True
+
+        time.sleep(0.2)
+
+    return False
+
 
 def get_today_text():
     return date.today().isoformat()
@@ -106,6 +132,7 @@ def save_count_state(state_file, day_text, count_number):
         file.write(f"date={day_text}\n")
         file.write(f"count={count_number}")
 
+
 def open_type_selection():
     wait_for_pixels(pixel1=(1024, 462, 0, 46, 18))  # home screen
     safe_sleep(0.3)
@@ -121,11 +148,13 @@ def open_type_selection():
 
     wait_for_pixels(pixel1=(1590, 155, 255, 255, 255))  # type selection
 
+
 def open_challenge():
     direct_click(1525, 508, clicks=1) # move to challenge
     direct_click(1524, 506, clicks=1)
 
     wait_for_pixels(pixel1=(1589, 151, 255, 255, 255)) # wait for challenge selection
+
 
 def open_raid():
     direct_click(1664, 500, clicks=1) # move to raid
@@ -133,9 +162,11 @@ def open_raid():
 
     wait_for_pixels(pixel1=(1589, 151, 255, 255, 255)) # wait for raid selection
 
+
 def create_room():
     direct_click(1584, 438, clicks=1)  # move to create room
     direct_click(1582, 438, clicks=1)
+
 
 def start_room():
     direct_click(1706, 452, clicks=1)  # move to start
@@ -211,6 +242,7 @@ def aizen_one_round():
 
     return found
 
+
 def aizen_100_macro():
     state_day, detect_count = load_count_state(AIZEN_STATE_FILE)
 
@@ -268,7 +300,6 @@ def shenron_setup():
 
     start_room()  # move to start
 
-
     print("Shenron setup done")
 
 
@@ -291,6 +322,7 @@ def shenron_one_round():
 
     return found
 
+
 def shenron_100_macro():
     state_day, detect_count = load_count_state(SHENRON_STATE_FILE)
 
@@ -302,7 +334,7 @@ def shenron_100_macro():
         found = shenron_one_round()
 
         if found:
-            detect_count += 1
+            detect_count += 0.5
             save_count_state(SHENRON_STATE_FILE, state_day, detect_count)
             print(f"Shenron trait shard detected {detect_count}/100")
         else:
@@ -313,6 +345,80 @@ def shenron_100_macro():
         safe_sleep(0.1)
 
     print("Shenron trait shard reached 100 today")
+
+
+def griffith_setup():
+    print("Griffith setup starting")
+
+    safe_sleep(1)
+    open_type_selection()  # room type selection
+    safe_sleep(0.3)
+
+    open_raid()  # open raid
+    safe_sleep(0.3)
+
+    direct_click(1267, 277, clicks=1) # move to Eclipse
+    direct_click(1265, 277, clicks=1)
+
+    safe_sleep(0.2)
+
+    direct_click(1447, 294, clicks=1)  # move to select act 4
+    direct_click(1445, 294, clicks=1)
+
+    safe_sleep(0.2)
+    create_room()  # move to create room
+    safe_sleep(0.2)
+
+    start_room()  # move to start
+
+    print("Griffith setup done")
+
+
+def griffith_one_round():
+    print("Griffith round starting")
+
+    safe_sleep(10)
+
+    wait_for_pixels(pixel1=(1639, 197, 248, 70, 71)) # pop up
+
+    direct_click(1639, 197, clicks=1)  # close pop up
+    direct_click(1638, 197, clicks=1)
+
+    wait_for_pixels(pixel1=(1298, 451, 132, 134, 0))  # wait for victory screen
+
+    safe_sleep(0.3)
+
+    found = check_for_pixels(
+        pixel1=(1334, 415, 252, 163, 0),
+        timeout=5
+    )  # check if griffith trait shard appeared
+
+    return found
+
+
+def griffith_100_macro():
+    state_day, detect_count = load_count_state(GRIFFITH_STATE_FILE)
+
+    griffith_setup()
+
+    while detect_count < 100:
+        check_stop()
+
+        found = griffith_one_round()
+
+        if found:
+            detect_count += 1
+            save_count_state(GRIFFITH_STATE_FILE, state_day, detect_count)
+            print(f"Griffith trait shard detected {detect_count}/100")
+        else:
+            print("Griffith trait shard not found, restarting round")
+
+        direct_click(1298, 450, clicks=1)  # move to replay
+        direct_click(1230, 450, clicks=1)
+        safe_sleep(0.1)
+
+    print("Griffith trait shard reached 100 today")
+
 
 def regular_challenge_setup():
     print("Regular challenge setup")
@@ -348,6 +454,7 @@ def regular_challenge_one_round():
 
     safe_sleep(0.1)
 
+
 def thirty_min_macro():
     print("30 minute macro")
     # your 30-minute code here
@@ -363,6 +470,7 @@ def thirty_min_macro():
 
     safe_sleep(0.1)
 
+
 while True:
     check_stop()
 
@@ -376,19 +484,25 @@ while True:
 
         save_count_state(AIZEN_STATE_FILE, today_text, 0)
         save_count_state(SHENRON_STATE_FILE, today_text, 0)
+        save_count_state(GRIFFITH_STATE_FILE, today_text, 0)
 
         aizen_100_macro()
         shenron_100_macro()
+        griffith_100_macro()
 
     else:
         _, aizen_count = load_count_state(AIZEN_STATE_FILE)
         _, shenron_count = load_count_state(SHENRON_STATE_FILE)
+        _, griffith_count = load_count_state(GRIFFITH_STATE_FILE)
 
         if aizen_count < 100:
             aizen_100_macro()
 
         if shenron_count < 100:
             shenron_100_macro()
+
+        if griffith_count < 100:
+            griffith_100_macro()
 
     last_30_min_run = None
 
